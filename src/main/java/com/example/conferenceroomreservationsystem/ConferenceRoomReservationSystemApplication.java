0b6cf180da7eed1893e50bc656215246ae646729
Przemek <Przemek@HP-Przemek.(none)>
2022-10-22 10:52:32 +0200

    init project

---
+++ src/main/java/com/example/conferenceroomreservationsystem/ConferenceRoomReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.conferenceroomreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenceRoomReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConferenceRoomReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/conferenceroomreservationsystem/ConferenceRoomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.conferenceroomreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceRoomReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
